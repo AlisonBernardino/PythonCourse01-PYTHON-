a1ffba741cd208da5a3388b0c851b4bde7af369a
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-06-16 06:23:46 -0300

    class09 file added

---
+++ exercises/class09.py
@@ -0,0 +1,20 @@
+#Class 09 - String manipulation
+# String cut technique
+sentence = 'Welcome to Python world'
+#My custom examples
+# Example 00 - Based on course example
+print(sentence[1])
+print(sentence[0:7])
+print(sentence[8:])
+
+#(01/10)
+
+#(02/10)
+#(03/10)
+#(04/10)
+#(05/10)
+#(06/10)
+#(07/10)
+#(08/10)
+#(09/10)
+#(10/10)
